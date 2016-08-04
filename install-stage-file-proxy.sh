3aa1ad070f4939d52b228da00907a47a609fcc49
Hugo <hugovk@users.noreply.github.com>
2016-08-04 13:42:52 +0000

    Helper script for installing stage_file_proxy

---
+++ install-stage-file-proxy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo ""
+echo "Make sure your local.settings.php contains this:"
+echo "
+$conf['running_on_localhost'] = TRUE;"
+echo ""
+echo "And make sure your settings.php contains this:"
+echo "
+if (!defined('PANTHEON_ENVIRONMENT') && variable_get('running_on_localhost', FALSE)) {
+  $conf['stage_file_proxy_origin'] = 'http://www.example.fi'; // no trailing slash
+}
+"
+echo ""
+
+drush dl stage_file_proxy --no
+drush en stage_file_proxy --yes
